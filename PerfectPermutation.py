931398a3c278de5c0de7df9b89bb094aefc4c9b6
Danial Rizvi <85439664+DanielRizvi@users.noreply.github.com>
2025-08-05 17:30:56 +0530

    Add files via upload

---
+++ PerfectPermutation.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+#if n is odd
+if n%2 == 1:
+    print(-1)
+else:
+    ans = []
+    for i in  range(2,n+1,2):
+        ans.extend([i,i-1])
+
+    #unzipping a list
+    print(* ans)
