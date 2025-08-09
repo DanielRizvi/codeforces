3749c544300b82a2217cc244e9c206508f0c6703
Danial Rizvi <85439664+DanielRizvi@users.noreply.github.com>
2025-08-09 19:48:17 +0530

    Add files via upload

---
+++ Cupboards.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+#inital counts
+left = {0:0,1:0} #state and its counts
+right = {0:0,1:0}
+
+for i in range(n):
+    #state of the cupboard
+
+
+    lft, rght = list(map(int,input().rstrip().split()))
+    left[lft] += 1
+    right[rght] += 1
+
+    #which is lesser in each case
+total=0
+if left[0] > left[1]:
+    total += left[1]
+else:
+    total += left[0]
+
+if right[0] > right[1]:
+    total += right[1]
+else:
+    total += right[0]
+
+print(total)
