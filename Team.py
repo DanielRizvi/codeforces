d2ffe86bc0c192f3129ea820ffdf6b52578ec408
Danial Rizvi <85439664+DanielRizvi@users.noreply.github.com>
2025-08-20 08:52:49 -0700

    Add files via upload

---
+++ Team.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+ans = 0
+
+for _ in range(n):
+    lst = list(map(int,input().rstrip().split()))
+    #assign petyas vasya and tonya
+    petya = lst[0]
+    vasya = lst[1]
+    tonya = lst[2]
+    #sum of all these 
+    total = petya + vasya + tonya 
+    if total >= 2:
+        ans += 1
+    else:
+        continue 
+print(ans)
