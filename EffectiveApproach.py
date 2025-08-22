0d2be331105c0fc8c0f849600347bea22be40d60
Danial Rizvi <85439664+DanielRizvi@users.noreply.github.com>
2025-08-22 06:30:41 -0700

    Add files via upload

---
+++ EffectiveApproach.py
@@ -0,0 +1,25 @@
+n = int(input())
+lst = list(map(int,input().rstrip().split()))
+
+m = int(input())
+queries = list(map(int,input().rstrip().split()))
+
+#dict 
+d = {}
+for i in range(n):
+    #element : i index 
+    d[lst[i]] = i
+
+vasya = 0
+petya = 0
+
+for q in queries:
+    index = d[q]
+    #vasya index - no of comparisons 
+    v = d[q]
+    #petya index - no of comparisons 
+    p = n - d[q]
+
+    vasya += v
+    petya += p
+print(vasya, petya)
